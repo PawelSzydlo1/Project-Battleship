6958fa2216218aef798b05bc45ac5325c7204e18
bartoszszlezal <barmikszl@gmail.com>
2020-05-13 19:41:28 +0200

    Wybranie punktu początkowego do ustawienia statku oraz utworzenie funckji losującej kierunek dalszego ustawiania okrętu.

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from logic import *
 
 #inicjowanie planszy komputera
 AI_list=[[] for i in range(0,10,1)]
@@ -8,6 +9,25 @@ for i in range(0,10):
         Player_list[i].append(0)
         AI_list[i].append(0)
 
-print(AI_list)
-print(Player_list)
 
+
+#wypełnienie planszy gracza
+
+def draw_player():
+    #Statki zajmują 20 pól, counter zlicza ustawione pola
+    counter=0
+    for i in range(0, 10):
+        for j in range(0, 10):
+            Player_list[i][j]=0
+
+
+
+    while counter==0: #ustawianie czteromasztowca
+        counter+=1
+        x1,y1=random_position(0,9)
+        Player_list[x1][y1]=1
+
+    for i in range(0, 10):
+        print(Player_list[i])
+
+draw_player()
--- logic.py
+++ logic.py
@@ -1,1 +1,48 @@
+import random
+
+#losowanie pozycji
+def random_position(a, b):
+    x = random.randint(a, b)
+    y = random.randint(a, b)
+    return x, y
+
+#losowanie kierunku ustawiania statku
+
+def random_direction(index1, index2, amount):
+    # 0-północ 1-wschód, 2-południe, 3-zachód
+    x=random.randint(0,3)
+    if index1<amount or index2>9-amount:
+        #przypadki narożników
+        if index2<amount:
+            if index1<amount:
+                while x==3 or x==0:
+                    x=random.randint(0,3)
+            elif index1>9-amount:
+                while x==2 or x==3:
+                    x=random.randint(0,3)
+        elif index2>9-amount:
+            if index1<amount:
+                while x==0 or x== 1:
+                    x=random.randint(0,3)
+            elif index1 > 9-amount:
+                while x==2 or x==1:
+                    x= random.randint(0,3)
+        else: # przypadek gdy kolumna jest odpowiednia
+            if index1<amount:
+                while x==0:
+                    x=random.randint(0,3)
+            elif index1 >9-amount:
+                while x==2:
+                    x=random.randint(0,3)
+    # dobry wiersz i zła kolumna
+    elif (index1>=amount and index1 <=9-amount and (index2<amount or index2 >9-amount)):
+        if index2 < amount:
+            while x==3:
+                x=random.randint(0,3)
+        elif index2 > 9-amount:
+            while x==1:
+                x=random.randint(0,3)
+
+    return x
+
 
