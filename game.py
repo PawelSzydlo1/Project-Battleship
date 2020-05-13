3f8e26c2aa0d7ca7b01845c8f8e6e9e89afc60b2
bartoszszlezal <barmikszl@gmail.com>
2020-05-13 18:36:59 +0200

    Utworzenie plansz gracza i AI

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
 
+#inicjowanie planszy komputera
+AI_list=[[] for i in range(0,10,1)]
+#inicjowanie planszy gracza
+Player_list=[[] for i in range (0,10,1)]
+for i in range(0,10):
+    for j in range(0,10):
+        Player_list[i].append(0)
+        AI_list[i].append(0)
+
+print(AI_list)
+print(Player_list)
+
