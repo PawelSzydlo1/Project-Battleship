e3eac256088f6dc2b1e7519165f9b88e5dfcf5eb
bartoszszlezal <barmikszl@gmail.com>
2020-05-13 18:01:21 +0200

    Dodanie wymiarów okna i przycisków.

--- constant.py
+++ constant.py
@@ -1,1 +1,18 @@
+#główne wymiary okna
+WIDTH=1000
+HEIGHT=1000
+#Kolory
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+RED=(255,0,0)
+GREEN=(0,255,0)
+BLUE=(0,0,255)
+#wymiary przycisków
+BUTTON_WIDTH=250
+BUTTON_HEIGHT=125
+#wymiary przycisków do statków
+BUTTON_WIDTH_1=30
+BUTTON_HEIGHT_1=30
+
+
 
