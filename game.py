9d19d432aa1cc8472bd57fa45074050af8bebddd
bartoszszlezal <barmikszl@gmail.com>
2020-05-14 16:18:22 +0200

    Dodanie funkcji uzupełniającej planszę AI

--- game.py
+++ game.py
@@ -71,6 +71,65 @@ def draw_player():
     for i in range(0, 10):
         print(Player_list[i])
 
+
+
+#uzupełnienie planszy komputera
+def draw_AI():
+    # counter zlicza ustawione statki
+    counter = 0
+    for i in range(0, 10):
+        for j in range(0, 10):
+            AI_list[i][j] = 0
+
+    while counter == 0:  # ustawianie czteromasztowca
+        counter += 1
+        x1, y1 = random_position(0, 9)
+        AI_list[x1][y1] = 1
+        # ustawianie czteromasztowca
+        set_4ship(AI_list, x1, y1)
+        # ustawianie sąsiadów okrętu na wartość 2 zapobiega ustawianiu innych okrętów dookoła
+        put_2(AI_list)
+    # ustawianie trójmasztowców
+    while counter < 3:
+        x2, y2 = random_position(0, 9)
+        o = Neighbour(AI_list, x2, y2, 1)
+        if o.neighbour() == 0 and AI_list[x2][y2] == 0:
+            AI_list[x2][y2] = 1
+            s = Ship_3(AI_list, x2, y2, random_direction(x2, y2, 2))
+            if s.set_ship():
+                counter += 1
+                # uzupełnienie sąsiadów wartością 2
+                put_2(AI_list)
+            else:
+                AI_list[x2][y2] = 0
+    # ustawianie dwumasztowców
+    while counter < 6:
+        x3, y3 = random_position(0, 9)
+        o1 = Neighbour(AI_list, x3, y3, 1)
+        if o1.neighbour() == 0 and AI_list[x3][y3] == 0:
+            AI_list[x3][y3] = 1
+            s1 = Ship_2(AI_list, x3, y3, random_direction(x3, y3, 1))
+            if s1.set_ship():
+                counter += 1
+                put_2(AI_list)
+            else:
+                AI_list[x3][y3] = 0
+
+    # ustawianie jednomasztowców
+    while counter < 10:
+        x4, y4 = random_position(0, 9)
+        o2 = Neighbour(AI_list, x4, y4, 1)
+        if o2.neighbour() == 0 and AI_list[x4][y4] == 0:
+            AI_list[x4][y4] = 1
+            counter += 1
+            put_2(AI_list)
+
+
+
+    for i in range(0, 10):
+        print(AI_list[i])
+
+
 draw_player()
-
-
+print("\n\n\n\n\n\n")
+draw_AI()
