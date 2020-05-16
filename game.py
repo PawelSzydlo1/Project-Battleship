b993f1648d063bd617af3fa065bfe33e60ef5d07
bartoszszlezal <barmikszl@gmail.com>
2020-05-16 14:31:27 +0200

    Dodanie kilku wyrażeń lambda do obsługi list

--- game.py
+++ game.py
@@ -20,11 +20,10 @@ AI_list=[[] for i in range(0,10,1)]
 Player_list=[[] for i in range (0,10,1)]
 for i in range(0,10):
     for j in range(0,10):
-        Player_list[i].append(0)
-        AI_list[i].append(0)
+        Player_list[i].append((lambda x:x)(0))
+        AI_list[i].append((lambda x:x)(0))
 
-
-
+equal=lambda x:x
 #wypełnienie planszy gracza
 
 def draw_player():
@@ -32,12 +31,12 @@ def draw_player():
     counter=0
     for i in range(0, 10):
         for j in range(0, 10):
-            Player_list[i][j]=0
+            Player_list[i][j]=equal(0)
 
     while counter==0: #ustawianie czteromasztowca
         counter+=1
         x1,y1=random_position(0,9)
-        Player_list[x1][y1]=1
+        Player_list[x1][y1]=equal(1)
         #ustawianie czteromasztowca
         set_4ship(Player_list,x1,y1)
         #ustawianie sąsiadów okrętu na wartość 2 zapobiega ustawianiu innych okrętów dookoła
@@ -47,35 +46,37 @@ def draw_player():
         x2,y2=random_position(0,9)
         o=Neighbour(Player_list,x2,y2,1)
         if o.neighbour()==0 and Player_list[x2][y2]==0:
-            Player_list[x2][y2]=1
+            Player_list[x2][y2]=equal(1)
             s=Ship_3(Player_list,x2,y2,random_direction(x2,y2,2))
             if s.set_ship():
                 counter+=1
                 #uzupełnienie sąsiadów wartością 2
                 put_2(Player_list)
             else:
-                Player_list[x2][y2]=0
+                Player_list[x2][y2]=equal(0)
     #ustawianie dwumasztowców
     while counter < 6:
         x3,y3=random_position(0,9)
         o1=Neighbour(Player_list,x3,y3,1)
         if o1.neighbour()==0 and Player_list[x3][y3]==0:
-            Player_list[x3][y3]=1
+            Player_list[x3][y3]=equal(1)
             s1=Ship_2(Player_list,x3,y3,random_direction(x3,y3,1))
             if s1.set_ship():
                 counter+=1
                 put_2(Player_list)
             else:
-                Player_list[x3][y3]=0
+                Player_list[x3][y3]=equal(0)
 
     #ustawianie jednomasztowców
     while counter <10:
         x4,y4=random_position(0,9)
         o2=Neighbour(Player_list,x4,y4,1)
         if o2.neighbour()==0 and Player_list[x4][y4]==0:
-            Player_list[x4][y4]=1
+            Player_list[x4][y4]=equal(1)
             counter+=1
             put_2(Player_list)
+
+
 
 #uzupełnienie planszy komputera
 def draw_AI():
@@ -83,12 +84,12 @@ def draw_AI():
     counter = 0
     for i in range(0, 10):
         for j in range(0, 10):
-            AI_list[i][j] = 0
+            AI_list[i][j] = equal(0)
 
     while counter == 0:  # ustawianie czteromasztowca
         counter += 1
         x1, y1 = random_position(0, 9)
-        AI_list[x1][y1] = 1
+        AI_list[x1][y1] = equal(1)
         # ustawianie czteromasztowca
         set_4ship(AI_list, x1, y1)
         # ustawianie sąsiadów okrętu na wartość 2 zapobiega ustawianiu innych okrętów dookoła
@@ -98,33 +99,33 @@ def draw_AI():
         x2, y2 = random_position(0, 9)
         o = Neighbour(AI_list, x2, y2, 1)
         if o.neighbour() == 0 and AI_list[x2][y2] == 0:
-            AI_list[x2][y2] = 1
+            AI_list[x2][y2] = equal(1)
             s = Ship_3(AI_list, x2, y2, random_direction(x2, y2, 2))
             if s.set_ship():
                 counter += 1
                 # uzupełnienie sąsiadów wartością 2
                 put_2(AI_list)
             else:
-                AI_list[x2][y2] = 0
+                AI_list[x2][y2] = equal(0)
     # ustawianie dwumasztowców
     while counter < 6:
         x3, y3 = random_position(0, 9)
         o1 = Neighbour(AI_list, x3, y3, 1)
         if o1.neighbour() == 0 and AI_list[x3][y3] == 0:
-            AI_list[x3][y3] = 1
+            AI_list[x3][y3] = equal(1)
             s1 = Ship_2(AI_list, x3, y3, random_direction(x3, y3, 1))
             if s1.set_ship():
                 counter += 1
                 put_2(AI_list)
             else:
-                AI_list[x3][y3] = 0
+                AI_list[x3][y3] = equal(0)
 
     # ustawianie jednomasztowców
     while counter < 10:
         x4, y4 = random_position(0, 9)
         o2 = Neighbour(AI_list, x4, y4, 1)
         if o2.neighbour() == 0 and AI_list[x4][y4] == 0:
-            AI_list[x4][y4] = 1
+            AI_list[x4][y4] = equal(1)
             counter += 1
             put_2(AI_list)
 
